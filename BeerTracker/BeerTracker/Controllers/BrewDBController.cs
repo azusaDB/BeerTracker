618a3dbfabde5f1b5e238bfcb1550442ceb24dbd
Sunny Wu <etsunny@gmail.com>
2017-10-31 05:37:44 +0800

    jQuery strucures, head buildings and subpages

--- BeerTracker/BeerTracker/Controllers/BrewDBController.cs
+++ BeerTracker/BeerTracker/Controllers/BrewDBController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using ZenOfBeer.BreweryDb.Pcl.Public;
+//using ZenOfBeer.BreweryDb.Pcl.Public;
 using System.Threading.Tasks;
 using BeerTracker.Models;
 
