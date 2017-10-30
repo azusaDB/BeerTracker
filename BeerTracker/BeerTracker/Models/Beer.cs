d085886633a26c20cf6ea8ae090f1259b0978c56
Caleb <caleb.mccann@bellevuecollege.edu>
2017-10-30 15:59:01 -0700

    added Beer class

---
+++ BeerTracker/BeerTracker/Models/Beer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerTracker.Models
+{
+    public class Beer
+    {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string shortName { get; set; }
+            public decimal abv { get; set; }
+            public int breweryId { get; set; } //known as styleId
+            public string breweryName { get; set; }
+            public string image_small { get; set; }
+            public string image_large { get; set; }
+            public string description { get; set; }
+            public decimal ibu { get; set; }
+    }
+}
