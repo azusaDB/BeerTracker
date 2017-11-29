caf8978e7cb47b22e1f7ee81ec9bb959291bc1a5
caleb.mccann <caleb.mccann@N252-006>
2017-11-28 19:05:16 -0800

    test

---
+++ BeerTracker/BeerTracker/scripts/tests.js
@@ -0,0 +1,8 @@
+﻿QUnit.test("hello test", function (assert) {
+    assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("Random Beer Call", function (assert) {
+    var foo = randomBeer();
+    assert.ok(randomBeer() == true, "Passed!");
+});
